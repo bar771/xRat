68317ad637075db73834e512084c22419f9f850a
bar771 <bar771@users.noreply.github.com>
2017-01-08 19:08:28 +0200

    Update Client.java

--- RAT_Client/src/com/rat/hacker/Client.java
+++ RAT_Client/src/com/rat/hacker/Client.java
@@ -1,5 +1,10 @@
 package com.rat.hacker;
 
+/**
+ * @author Bar771
+ * https://Bar771.tumblr.com
+ */
+
 import java.net.Socket;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
